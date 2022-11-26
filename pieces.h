d0769dcdc59dc2e32891389064a47b0732f2898a
a22fu <a22fu@uwaterloo.ca>
2022-11-25 23:55:17 -0500

    finished pieces.h

--- pieces.h
+++ pieces.h
@@ -12,6 +12,8 @@ class Piece{
     char color;
     coord position;
     public:
+        Piece(char c, coord pos);
+        virtual ~Piece();        
         virtual void move(coord pos);
         virtual coord getPosition();
         virtual char getColor();
@@ -20,7 +22,45 @@ class Piece{
 };
 
 class Rook : Piece {
+    bool moved = false;
     public:
+        Rook(char c, coord pos);
+        void move(coord pos);
+        coord getPosition();
+        char getColor();
+        bool validMove(coord pos);
+        bool hasMoved();
+        void castle();
+};
+
+class King : Piece {
+    bool moved = false;
+    public:
+        King(char c, coord pos);
+        void move(coord pos);
+        coord getPosition();
+        char getColor();
+        bool validMove(coord pos);
+        bool hasMoved();
+        void castle();
+};
+
+
+class Pawn : Piece {
+    bool moved = false;
+    public:
+        Pawn(char c, coord pos);
+        void move(coord pos);
+        coord getPosition();
+        char getColor();
+        bool validMove(coord pos);
+        bool hasMoved();
+        std::string promote(std::string piece);
+};
+
+class Bishop : Piece {
+    public:
+        Bishop(char c, coord pos);
         void move(coord pos);
         coord getPosition();
         char getColor();
@@ -28,4 +68,23 @@ class Rook : Piece {
         bool hasMoved();
 };
 
+class Knight : Piece {
+    public:
+        Knight(char c, coord pos);
+        void move(coord pos);
+        coord getPosition();
+        char getColor();
+        bool validMove(coord pos);
+        bool hasMoved();
+};
+
+class Queen : Piece {
+    public:
+        Queen(char c, coord pos);
+        void move(coord pos);
+        coord getPosition();
+        char getColor();
+        bool validMove(coord pos);
+        bool hasMoved();
+};
 #endif
