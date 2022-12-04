552369fbfcf9dc688af4ceccc11dccea1c5764a5
a22fu <a22fu@uwaterloo.ca>
2022-12-03 20:13:56 -0500

    minor changes

--- computer.h
+++ computer.h
@@ -3,33 +3,38 @@
 
 #include <string>
 #include "board.h"
-
-class Board;
+#include <vector>
 
 class Computer{
     Board* b;
-    vector<string> allValidMoves;
+    vector<string> allValidMoves;  //strings formatted "startcoordsendcoords" ("a4e4")
     public:        
         Computer(Board* b);
         virtual ~Computer(){};
-        virtual move();
+        virtual void move() = 0;
         
         
         }
 
 class Computer1 : public Computer{
-
+    
+    public:
+        void move();
 }
 
 class Computer2 : public Computer{
-
+    vector<string> captureCheckMoves;
+    public:
+        void move();
 }
 
 class Computer3 : public Computer{
-
+    public:
+        void move();
 }
 
 class Computer4 : public Computer{
-
+    public:
+        void move();
 }
 #endif
